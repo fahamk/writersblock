bc2a04bbb298f8117137c5b1cb878903d42de6f3
fahamk-ca <fahamk@ca.ibm.com>
2018-03-21 18:07:59 -0400

    Sign up html page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var passport = require('passport');
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
